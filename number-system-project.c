3ad2eae5fbbf1365328fff60724f1124a7744e86
Fahry0007 <fahrymohamed5@gmail.com>
2025-11-06 20:40:53 +0530

    Add files via upload

---
+++ number-system-project.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Type The Number: ");
+    scanf("%d", &a);
+    printf(" Octal Number:%o\n", a);
+    printf(" HexadecimalNumber:%x\n", a);
+    return 0;
+}
